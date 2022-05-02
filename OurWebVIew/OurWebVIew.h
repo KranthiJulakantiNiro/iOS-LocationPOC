4b721f29a16860edd3b2fb3226ed6c50e73f8d1f
KranthiJulakantiNiro <kranthi@niro.money>
2022-05-02 15:47:56 +0530

    Initial Commit

---
+++ OurWebVIew/OurWebVIew.h
@@ -0,0 +1,18 @@
+//
+//  OurWebVIew.h
+//  OurWebVIew
+//
+//  Created by Kranthi Kumar Julakanti on 02/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OurWebVIew.
+FOUNDATION_EXPORT double OurWebVIewVersionNumber;
+
+//! Project version string for OurWebVIew.
+FOUNDATION_EXPORT const unsigned char OurWebVIewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OurWebVIew/PublicHeader.h>
+
+
